e10ad608e82af07d08b66386ba67cd166c9bfe39
LinkClinton <LinkClinton@outlook.com>
2019-11-25 21:59:44 +0800

    Start Triangles Mesh.

---
+++ LRTR-Lab/Scenes/Components/TrianglesMesh.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../Component.hpp"
+
+namespace LRTR {
+
+	class TrianglesMesh : public Component {
+		
+	};
+	
+}
--- LRTR-Lab/Scenes/Shapes/SceneProperty.cpp
+++ LRTR-Lab/Scenes/Shapes/SceneProperty.cpp
@@ -9,7 +9,9 @@ LRTR::SceneProperty::SceneProperty()
 {
 	addComponent(std::make_shared<CoordinateSystem>());
 	addComponent(std::make_shared<CameraGroup>());
-	addComponent(std::make_shared<LinesGrid>(RectangleF(-5, -5, 5, 5), 10, 10));
+	addComponent(std::make_shared<LinesGrid>(RectangleF(-5, -5, 5, 5), 10, 10,
+		Vector3f(1, 0, 0), Vector3f(0, 1, 0),
+		Vector3f(0, 0, -0.001f)));
 }
 
 auto LRTR::SceneProperty::typeName() const noexcept -> std::string
